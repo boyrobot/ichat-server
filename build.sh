ee109c5580f1daee9bc031555690e8ee59fc7f0b
boyrobot <44001185+boyrobot@users.noreply.github.com>
2023-04-03 02:09:16 +0000

    0

--- build.sh
+++ build.sh
@@ -12,3 +12,7 @@ docker-compose up
 install_online.sh
 delete.sh
 restart.sh
+
+cd /workspaces/ichat-server ; /usr/bin/env /opt/java/11.0.14/bin/java @/tmp/cp_2m630vzp1wks7eban7u4zqz4h.argfile com.ichat.Application
+
+
