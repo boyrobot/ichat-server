5b54feef07b51eb88a455d3ac433d70a45a0d2d0
boyrobot <44001185+boyrobot@users.noreply.github.com>
2023-04-03 03:56:11 +0000

    0

--- build.sh
+++ build.sh
@@ -16,11 +16,17 @@ docker login --username=jianboy -p xx
 docker push jianboy/ichat:latest
 docker pull jianboy/ichat:latest
 
+cd script/froent
+git clone https://github.com/boyrobot/ichat-weixin
+
+docker build  -f Dockerfile -t jianboy/ichat-f:latest . 
+
+docker login --username=jianboy -p iNs3bIQXWcUDhxQVppZm
+docker push jianboy/ichat-f:latest
+docker pull jianboy/ichat-f:latest
 
 install_online.sh
 delete.sh
 restart.sh
 
 cd /workspaces/ichat-server ; /usr/bin/env /opt/java/11.0.14/bin/java @/tmp/cp_2m630vzp1wks7eban7u4zqz4h.argfile com.ichat.Application
-
-
